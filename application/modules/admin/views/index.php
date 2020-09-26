ace451143e2eb3c474ae0e93486c5d08b7b43c65
codingindoID <wearedevev@gmail.com>
2020-09-26 20:59:23 +0700

    edit index admin

--- application/modules/admin/views/index.php
+++ application/modules/admin/views/index.php
@@ -1,28 +1,12 @@
-  <!-- high chart -->
-  <link rel="stylesheet" href="<?php echo base_url().'assets/highchart/high.css' ?>"/>
-  <script src="<?php echo base_url('assets/highchart/') ?>highcharts.js"></script>
-  <script src="<?php echo base_url('assets/highchart/') ?>exporting.js"></script>
-  <script src="<?php echo base_url('assets/highchart/') ?>export-data.js"></script>
-  <script src="<?php echo base_url('assets/highchart/') ?>accessibility.js"></script>
-  <!-- high chart -->
-
-  <style type="text/css">
-    /*.center {
-text-align: center;
-vertical-align: middle;
-line-height: 200px;    
-}*/
-  </style>
-
   <div class="row">
     <div class="col">
       <div class="col-md-8 col-xs-12">
         <div class="small-box bg-purple" style="height: 250px;" >
          <div class="inner">
-         <h3 class="center"><span id="tx_total">50</span></h3>
+           <h3 class="center"><span id="tx_total">50</span></h3>
 
-        </div>
-        <div class="icon">
+         </div>
+         <div class="icon">
           <i class="ion ion-pie-graph"></i>
         </div>
       </div>
@@ -64,28 +48,3 @@ line-height: 200px;
 
 </div>
 <!-- end data -->
-
-<!-- MY -->
-<script src="<?php echo base_url().'assets/'?>bower_components/jquery/dist/jquery.min.js"></script>
-<script src="<?php echo base_url().'assets/my_js/jsdashboard.js'?>"></script>
-
-
-
-
-<!-- chart -->
-<!--       <div class="col-lg-6 col-md-6 col-xs-12">
-
-        <div class="box" style="padding: 5px;">
-        <div class="form-group">
-        <select class="form-control" style="width:100px;margin-left: 10px;" id="grafik_tahun">
-            <option value=""/>
-        </select>
-        </div>
-        <figure class="highcharts-figure">
-          <div id="container"></div>
-          <p class="highcharts-description" style="padding: 10px;">
-            Basis data terdiri dari kunjungan kendaraan Rutin dan Non rutin yang di hitung berdasarkan data inputan dari sistem 
-          </p>
-        </figure>
-      </div>  
-    </div> -->
